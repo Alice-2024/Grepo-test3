d7f46a2785431b70db69b3216851b14b5f9a9d16
Alice-2024 <85333139+Alice-2024@users.noreply.github.com>
2025-03-05 16:31:32 +0800

    Update Program.cs

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");//bb11112222
-Console.WriteLine("Hello, World!");//11
+Console.WriteLine("Hello, World!");//1111111
 Console.WriteLine("Hello, World!");
 //2223333aaaaabbb3333344455
 Console.WriteLine("Hello, World!");
