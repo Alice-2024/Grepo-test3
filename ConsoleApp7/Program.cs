7e44d73ab1a1b816bb0eaa06abc767093e6fc691
Alice Lei <v-qilei@microsoft.com>
2025-04-03 06:31:17 +0000

    Remove redundant Hello World statements
    Deleted multiple instances of `Console.WriteLine("Hello, World!");` from `Program.cs` to clean up the code.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -19,6 +19,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
