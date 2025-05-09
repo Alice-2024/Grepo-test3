db4c6c7c0efb67fabed7505250ddd9db00207cfc
Alice <v-qilei@microsoft.com>
2025-05-09 07:57:32 +0000

    Remove redundant Console.WriteLine statements
    Deleted multiple instances of `Console.WriteLine("Hello, World!");` from `Program.cs` to clean up the code.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,8 +10,4 @@ Console.WriteLine("Hello, World!");
 //aaaaabbbbbbbb1111
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
