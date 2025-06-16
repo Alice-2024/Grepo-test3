153c8c71d5672c16fe49c414d548376dfa036b95
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-06-16 06:31:14 +0000

    commit message

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -7,9 +7,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-//11111aaaa
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//aaaaa
-//bbbbb
