7b0796be4da9ffb7672c16add8477cfb41cdea31
Alice-2024 <85333139+Alice-2024@users.noreply.github.com>
2025-07-10 16:34:44 +0800

    Update Program.cs

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,9 +4,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-//1111aaaa
-//222bbbbb333333333333cccccdddd
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//aaaa22223333
