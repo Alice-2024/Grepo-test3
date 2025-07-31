8a4e4cb86ffc3a5fe55f607c7ea2110e8d2d68de
v-qilei <v-qilei@DESKTOP-OF8NDPD>
2025-07-31 15:32:36 +0800

    Refactor Console.WriteLine usage and add comments
    Removed a duplicate Console.WriteLine statement and added multiple instances of it. Introduced two new comment lines for clarity.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,6 +4,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//222222222222
-//aa1111
+
