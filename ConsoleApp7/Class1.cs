7ee29868b9fdfccbf8f1978ea66cc0f34222791a
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-15 06:33:42 +0000

    **Remove redundant comments**
      Removed two commented-out lines in `Class1.cs` containing `Console.WriteLine("Hello, World!");`. These lines were unnecessary and did not contribute to the code's functionality.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -9,8 +9,6 @@
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
       
     }
 }
