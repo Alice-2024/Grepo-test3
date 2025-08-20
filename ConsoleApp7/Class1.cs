ecffe1b759b26fa74cdf6b4814a41ddf1e9ba560
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-20 07:51:23 +0000

    "Add Method1"
      Added a new method `Method1` to the `Class1.cs` file. The implementation details of the method are not provided in the diff. A commented-out line of code (`//Console.WriteLine("Hello, World!");`) remains unchanged and is included for context.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -9,6 +9,6 @@
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
-      
+       public void Method1()
     }
 }
