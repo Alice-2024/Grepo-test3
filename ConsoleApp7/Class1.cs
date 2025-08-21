ba40e44f33682f8f25c31f209adc32d9b646063a
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-21 09:13:59 +0000

    "Refactor comments"
      Removed three redundant commented-out `Console.WriteLine` statements in `Class1.cs` to clean up the code. Adjusted the position of the closing curly brace to improve formatting and structure.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -6,9 +6,7 @@
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
         //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
+        
       
     }
 }
