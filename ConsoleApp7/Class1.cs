cb31b410be43c8746a1df709999587da0c09b886
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-22 07:38:25 +0000

    1111

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -2,9 +2,11 @@
 {
     internal class Class1
     {
-        public int a = 1;
-        //Console.WriteLine("Hello, World!");
-        //Console.WriteLine("Hello, World!");
+        public void Method1()
+        {
+            // This is a method that does nothing
+            // It is just a placeholder for demonstration purposes
+        }
 
 
     }
