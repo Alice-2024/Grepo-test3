040603cbbd2c1bc19a316c417f90a6b1ec55f046
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-25 07:48:54 +0000

    'Add extra print'
      Added an additional `Console.WriteLine` statement to print
      the even numbers to the console. This ensures the same
      "Even Numbers" message and list are displayed multiple times.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -10,6 +10,7 @@
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+            Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
         }
 
 
