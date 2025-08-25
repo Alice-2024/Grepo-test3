82f29e01e398f8eb40c8b8bf83627936e0861a46
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-08-25 07:33:52 +0000

    Add Method1 to filter and display even numbers
    A new method, `Method1`, was added to the `Class1` class in the `ConsoleApp7` namespace. This method demonstrates the use of LINQ to filter a list of integers for even numbers and outputs the result to the console. The implementation includes a comment referencing the use of a LINQ method introduced in C# 10.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -2,8 +2,14 @@
 {
     internal class Class1
     {
-        
-        
-      
+        public void Method1()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+            // Using the new LINQ method introduced in C# 10
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+            Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+        }
+
+
     }
 }
