e2aec8480322820a473706e4d8529445347f5b6b
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-09-17 14:55:52 +0800

    Add comment about C# 9 record type usage
    A comment was added to highlight the use of a new record type introduced in C# 9. This change improves code documentation and readability without altering functionality.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -8,6 +8,7 @@
             // Using the new LINQ method introduced in C# 10
             var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+            // Using the new record type introduced in C# 9
         }
 
 
