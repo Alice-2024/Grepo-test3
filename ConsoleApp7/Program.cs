4d7ca102feceaf259f99f1010278d8a7d21602fa
v-qilei <v-qilei@CPC-vqile-I5IR6>
2025-10-10 15:11:59 +0800

    Refactor MyClass and add constant string
    Added a constant string `myString` with a descriptive value.
    Introduced new `Console.WriteLine` statements for output,
    including "Hello", "test1", and "test2". Removed redundant
    commented lines. Added multiple "Hello, World!" print
    statements at the end of the code.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -15,9 +15,6 @@ public class MyClass
         Console.WriteLine("Hello");
         Console.WriteLine("test1");
         Console.WriteLine("test2");
-        //11111
-        //2222
-        //333333333333333333333
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
