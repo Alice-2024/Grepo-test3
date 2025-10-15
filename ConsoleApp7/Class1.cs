e0783a5bf1ae009304079a65e443fd13edbfd3e4
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-10-15 15:17:11 +0800

    22

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -18,6 +18,7 @@
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
         }
 
     }
