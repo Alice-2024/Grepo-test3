41e8e1b171a109117a9b9d9f179010b2c3104efc
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-10-20 10:08:55 +0800

    222

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -31,6 +31,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        //AAAAAAAAAAAAAAAA
     }
 
 }
