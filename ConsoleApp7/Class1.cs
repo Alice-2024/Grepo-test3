0e718df2a94bc0c6256d7da1e5ce6a65fb67d2dd
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-10-23 09:41:34 +0800

    33

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -16,9 +16,10 @@
             // Using the new LINQ method introduced in C# 10
             var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
             Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
 
 
         }
