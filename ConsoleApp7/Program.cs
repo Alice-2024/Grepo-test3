e5ff9a632c974ddafc022b786480ae0a0bd8b166
v-qilei <v-qilei@CPC-vqile-F6A18>
2025-10-24 15:05:17 +0800

    'Add Hello, World!'
      Added an additional `Console.WriteLine("Hello, World!");` statement in the `MyClass` class within `Program.cs`.
      This increases the number of times "Hello, World!" is printed to the console without modifying existing functionality.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -29,7 +29,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-
+        Console.WriteLine("Hello, World!");
     }
 
 }
