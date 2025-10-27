45b8897c0c61b36809162ce289cbe4ecac1b81c2
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-10-27 14:06:37 +0800

    ''' 'Update MyClass'
      Added multiple `Console.WriteLine` statements for output, including `test4`, `test5`, `test6`, and `test7`. Removed unused comments (`//AAAAA`, `//BBBB`, `//CCCC`) and adjusted the placement of the closing brace for the class.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -20,9 +20,7 @@ public class MyClass
         Console.WriteLine("test5");
         Console.WriteLine("test6");
         Console.WriteLine("test7");
-        //AAAAA
-        //BBBB
-        //CCCC
+
     }
 
 }
