9d69d70b6e435482cc71bcbd8b392d958997c019
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-11-10 15:11:06 +0800

    'Remove duplicates'
      Removed redundant `Console.WriteLine("Hello, World!");` calls
      in the `Main` method of the `Program2` class. Retained only
      one instance to improve code clarity and reduce redundancy.

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -6,10 +6,6 @@
         public static void Main()
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
         }
     }
 
