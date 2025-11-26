f69183c73e68ffffb58d546a8a7ddc60b9fb270e
v-qilei <v-qilei@CPC-vqile-99E3Y>
2025-11-26 16:23:04 +0800

    'Add comment to MyClass'
      在Program.cs文件中的MyClass类中添加了注释`//AAAA`。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -25,6 +25,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        //AAAA
     }
 
 }
