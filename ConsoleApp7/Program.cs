c7b47ed2f2f5439aa4b160e9e3f48336713282b1
Alice Lei <v-qilei@microsoft.com>
2025-11-27 15:25:07 +0800

    11

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -30,6 +30,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 
 }
