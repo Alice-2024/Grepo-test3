5cadc1c226876d9e23723c56822d9d9ef0789302
Alice Lei <v-qilei@microsoft.com>
2025-11-27 10:17:05 +0800

    'Update MyClass'
      添加了 `Console.WriteLine("Hello!");`。
      删除了一行重复的 `Console.WriteLine("Hello, World!");`。
      在代码块末尾添加了注释 `//11111111111`。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -22,13 +22,14 @@ public class MyClass
         Console.WriteLine("test3");
         Console.WriteLine("test4");
         Console.WriteLine("test11");
+        Console.WriteLine("Hello!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        //11111111111
     }
 
 }
