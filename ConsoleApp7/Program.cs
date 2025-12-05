e3a082436be2f03f7e90425b26d3fb2312f959e5
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-12-05 16:54:47 +0800

    'Add test1205 output'
      Added a new `Console.WriteLine` statement in the `MyClass` class
      within the `Program.cs` file to output the string "test1205" to
      the console.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -25,6 +25,7 @@ public class MyClass
         Console.WriteLine("Hello!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("test1205");
 
     }
 
