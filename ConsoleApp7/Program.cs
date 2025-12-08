4514845c5b5d435db964478bad021c0aac32c0be
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-12-08 15:32:45 +0800

    'Add extra greeting'
      Added a new `Console.WriteLine("Hello, World!");` statement to the `MyClass` class in the `Program.cs` file. This change introduces an additional line of output to the console when the program runs.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -30,6 +30,7 @@ public class MyClass
         Console.WriteLine("Hello!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 
 }
