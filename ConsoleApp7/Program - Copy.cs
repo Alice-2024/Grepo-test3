ad0a7bf20ffbeff34dce904887e660d014e017d4
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-12-16 14:37:18 +0800

    #199

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -6,11 +6,7 @@
         public static void Main()
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("111");
         }
     }
 
