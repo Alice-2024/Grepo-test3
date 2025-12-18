a070003a3170b1c82ffae806b940b0dfb88d10f8
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-12-18 14:19:45 +0800

    '清理合并冲突标记'
      移除了类结尾处的注释代码和合并冲突标记，修正了类的闭合结构，提升了代码整洁性和可读性。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -24,12 +24,6 @@ public class MyClass
         Console.WriteLine("test11");
         Console.WriteLine("Hello!");
         Console.WriteLine("Hello, World!");
-        //11111
-        //2222
-<<<<<<< HEAD
-        //AAAA
-=======
-        //3333
->>>>>>> Test/1218
+
     }
 }
