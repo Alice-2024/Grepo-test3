8ab49c0fbf626575f2141736acd2fff2b6301b70
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@CPC-vqile-99E3Y>
2025-12-25 14:16:59 +0800

    222

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -27,6 +27,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
