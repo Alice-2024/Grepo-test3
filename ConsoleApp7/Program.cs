6a6eaf9c761c141f38925501b9dd244a0cb490bd
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@CPC-vqile-99E3Y>
2025-12-25 13:55:50 +0800

    Revert "#316"
    This reverts commit a3016a3bf4f3742e99e0544437129d37a1fb4ca8.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -34,6 +34,6 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         //1111
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+
     }
 }
