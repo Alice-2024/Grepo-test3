bd8ebb98f1a2b58e3d66e418fc49eb9a6c5f54bb
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-01-06 14:48:51 +0800

    '移除冗余代码'
      删除了整个 Program - Copy.cs 文件，其中包含重复的打印逻辑。并在 Program.cs 的 MyClass 类中移除了多余的 "Hello, World!" 输出语句，仅保留必要的 Console.WriteLine 调用。CodeFile1.cs 未做更改。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -23,12 +23,7 @@ public class MyClass
         Console.WriteLine("test4");
         Console.WriteLine("test11");
         Console.WriteLine("Hello!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        //AAAA
+        
     }
 
 }
