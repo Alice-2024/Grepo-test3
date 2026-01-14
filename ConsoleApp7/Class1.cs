e3849f5bd9a847eacd77f366d6b114e54340466e
v-qilei <v-qilei@microsoft.com>
2026-01-14 14:20:38 +0800

    '移除多余输出'
      删除了多次 "Hello, World!" 的控制台输出，方法现在在打印偶数和空行后结束，代码更加简洁。

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -19,12 +19,7 @@
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-        }
 
+
+        }
     }
-}
