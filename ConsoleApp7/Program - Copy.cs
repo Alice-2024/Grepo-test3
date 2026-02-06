a127ea7752c470744b063bf9e5108dc31267b23f
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-02-06 15:04:57 +0800

    '调整输出与注释'
      删除多余的Console.WriteLine行和注释，规范了代码格式和括号位置。

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -44,7 +44,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
