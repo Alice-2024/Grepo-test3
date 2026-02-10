718534a68ec19de90939a39926670ea8eeb548f6
Alice-2024 <85333139+Alice-2024@users.noreply.github.com>
2026-02-10 14:06:47 +0800

    Remove commented line from Program.cs

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -39,6 +39,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        //aaaaaaa
     }
 }
 
