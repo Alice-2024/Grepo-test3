3dfea7b2e9476809e050e72aa4900e8e41a1fa5a
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-02-27 16:00:43 +0800

    'Clean up test output'
      移除了Program.cs中MyClass类的测试输出和注释，保留了主要的打印语句，简化了控制台输出内容。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -26,12 +26,6 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-        //TEST1224
-        Console.WriteLine("Test1224");
-        Console.WriteLine("Test1224");
-        Console.WriteLine("Hello, World!");
-        //1111222233333
-        Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
