0508b38cc0e08d0567d8b02250e1c17a227782c0
Alice-2024 <85333139+Alice-2024@users.noreply.github.com>
2026-02-28 11:07:08 +0800

    Add comment in Program.cs

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -37,6 +37,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.BackgroundColor = ConsoleColor.Green;
+        //aaaaaaaaaaa
     }
 }
 
