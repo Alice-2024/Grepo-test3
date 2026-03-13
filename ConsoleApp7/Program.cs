09845bd32b7b0c391a85ddfb67c4fef22aed233b
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-03-13 14:09:57 +0800

    '精简输出代码'
      在 Program.cs 的 MyClass 类中，移除了多余的 Console.WriteLine("Hello, World!") 语句和注释，保留了部分输出，并设置了控制台背景色为绿色。

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -35,23 +35,6 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        //aaaaa
-        //1111
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        //aaaaaa
-        Console.WriteLine("Hello, World!");
-        //111111
-        //aaaaa
-        //22222222222
-        //bbbbbb
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
         Console.BackgroundColor = ConsoleColor.Green;
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
