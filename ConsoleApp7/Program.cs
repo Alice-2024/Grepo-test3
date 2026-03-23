0116c840ab50307a8f49049c655cd712212e261b
v-qilei <v-qilei@CPC-vqile-SLVQ5>
2026-03-23 10:32:54 +0800

    Program.cs remove comments
      移除了Program.cs中MyClass类的多余注释行，包括//test0323、//11和//aaaaaaaa，提升了代码可读性，未影响功能。
    
    That's the end of the commit message.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -33,8 +33,6 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         //test0323
-        //11
-        //aaaaaaaa
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
