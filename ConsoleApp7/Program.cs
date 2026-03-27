e71199314aef3493c18b09c35e0d5de51dba293f
v-qilei <v-qilei@CPC-vqile-C7HO0>
2026-03-27 16:05:59 +0800

    1111
    MyClass 增加输出
    
      在Program.cs的MyClass类中增加了一条Console.WriteLine("Hello, World!")语句，使输出多了一行"Hello, World!"。
    That's the end of the commit message.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -42,6 +42,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
 
