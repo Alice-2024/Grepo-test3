fb1a6daffd9ddaf2eac8a6b6b2a9ded397faafce
v-qilei <v-qilei@CPC-vqile-C7HO0>
2026-03-30 10:12:28 +0800

    Program.cs add print line
      在Program.cs的MyClass类中增加了一条Console.WriteLine("Hello, World!")语句，使"Hello, World!"的输出次数增加。
    
    That's the end of the commit message.
    
    MyClass 增加输出行
    
      在 Program.cs 的 MyClass 类中增加了一行 Console.WriteLine("Hello, World!")，使输出次数增加。
    That's the end of the commit message.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -47,6 +47,8 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
 
