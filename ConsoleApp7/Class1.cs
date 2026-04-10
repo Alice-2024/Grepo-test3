4c6c78d661d69c881ec9cb8fb18a9a47b5ecae45
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-04-10 16:29:53 +0800

    aaaaaaaaaa
    Added another Hello, World! print statement
    
    Increased the number of Console.WriteLine("Hello, World!") calls by one to print the message an additional time.
    
    That's the end of the commit message.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -36,7 +36,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("Hello, World!");
         }
 
     }
