dc933f685952e1d30196f4170a14f06de6c85a28
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-05-07 16:26:27 +0800

    Removed redundant Console.WriteLine statements
    Removed multiple Console.WriteLine calls from MyMethod in MyClass
    within Program.cs to clean up unnecessary output.
    
    That's the end of the commit message.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -6,24 +6,7 @@ public class MyClass
         char myChar = 'A';
         Console.WriteLine("Hello, World!");
         const string myString = "This is a constant string.";
-        Console.WriteLine("Hello");
-        Console.WriteLine("test1");
-        Console.WriteLine("test2");
-        Console.WriteLine("test3");
-        Console.WriteLine("test4");
-        Console.WriteLine("test5");
-        Console.WriteLine("test6");
-        Console.WriteLine("test7");
-        Console.WriteLine("test8");
-        Console.WriteLine("test9");
-        Console.WriteLine("test10");
-        Console.WriteLine("test1");
-        Console.WriteLine("test2");
-        Console.WriteLine("test3");
-        Console.WriteLine("test4");
-        Console.WriteLine("test11");
-        Console.WriteLine("Hello!");
-        Console.WriteLine("Hello, World!");
+
     }
 }
 
