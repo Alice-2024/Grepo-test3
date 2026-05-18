184d489557576c1a586614f2a6a1140bbabddf85
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-05-18 11:17:13 +0800

    Clean up comments and resolve merge conflicts
    Removed commented lines and merge conflict markers from MyClass in Program.cs.
    
    Added an extra Console.WriteLine("Hello, World!") statement.
    
    That's the end of the commit message.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -29,14 +29,7 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!")
-        //1111
-        //2222
-<<<<<<< HEAD
-        //AAAA
-=======
-        //3333
->>>>>>> Test/0518
+        Console.WriteLine("Hello, World!");
     }
 }
 
