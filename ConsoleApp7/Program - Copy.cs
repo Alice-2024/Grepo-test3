36f13e46b095fc2ef0ae39f34534a6159891af0a
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-06-08 10:02:20 +0800

    **Add console output for 'test5' after 'test4'**
    Added a new Console.WriteLine statement in Program - Copy.cs to print "test5" after the existing "test4" output.
    That's the end of the commit message.

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -23,6 +23,7 @@
             Console.WriteLine("test2");
             Console.WriteLine("test3");
             Console.WriteLine("test4");
+            Console.WriteLine("test5");
         }
     }
 }
