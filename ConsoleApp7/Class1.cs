f7dce3cde552f83accb0befa633cb491206129d4
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-06-15 14:17:16 +0800

    Add extra "Hello, World!" output and remove comment
    Removed a commented line (//AAAAAAA) and added an additional Console.WriteLine("Hello, World!") statement after multiple blank lines, resulting in two consecutive outputs of "Hello, World!".
    
    That's the end of the commit message.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -21,6 +21,7 @@
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
             Console.WriteLine("Hello, World!");
             //AAAAAAA
+            Console.WriteLine("Hello, World!");
         }
 
     }
