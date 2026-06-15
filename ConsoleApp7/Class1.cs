ab5c62d7cb97d69742adbf50a1b4ee52bb8fd7da
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-06-15 14:17:16 +0800

    888

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -21,6 +21,8 @@
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
             Console.WriteLine("Hello, World!");
             //AAAAAAA
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
 
     }
