438e9ffc770120b2bc2067aca991ad4497852172
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-07-27 14:57:59 +0800

    Program.cs
    Reduced redundant Console.WriteLine("Hello, World!") statements to simplify output. Added a comment //222 at the end of the method.
    
    That's the end of the commit message.
    #478

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -32,22 +32,6 @@ public class MyClass
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");//222
     }
 
 }
