d3e6554416e2fde34ec9fa6f1e247c9d8e831984
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-07-31 11:23:58 +0800

    Program - Copy.cs
    Replaced comment //22222222222 with //test auto resolve merge conflict to clarify the purpose of the comment.
    
    That's the end of the commit message.

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -43,7 +43,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             //test auto resolve merge conflict
-            //22222222222
+            //test auto resolve merge conflict
             //EEE
             //FFF
             //test auto resolve merge conflict
