04a2d6bc36434b7caa33be6dc574f502d1554cba
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2026-07-31 11:10:44 +0800

    Program - Copy.cs
    Replaced the previous comment //AAAA with a new comment //a new comment in the middle of the code.
    
    That's the end of the commit message.

--- ConsoleApp7/Program - Copy.cs
+++ ConsoleApp7/Program - Copy.cs
@@ -35,7 +35,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            //AAAA
+            //a new comment in the middle of the code
             //BBB
             //CCC
             Console.WriteLine("Hello, World!");
